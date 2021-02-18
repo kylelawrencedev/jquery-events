722140104b737ab9c571e6d4a1255d9bc1aa7285
kylelawrencedev <kylelawrence19@gmail.com>
2021-02-18 14:59:52 +0000

    Lesson:jQuery Events, Effects

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
     $("#stream1_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream1").addClass('highlight_stream');
+        $(".stream1").show();
+        $(".stream1").show('slow');
+        $(".stream1").show('medium');
+        $(".stream1").show('fast');
+        $(".stream1").show(1000);
+        or 
+        $(".stream1").hide();
+        $(".stream1").hide('slow');
+        $(".stream1").hide('medium');
+        $(".stream1").hide('fast');
+        $(".stream1").hide(1000);
+        or         
+        $(".stream1").toggle();
+        $(".stream1").toggle(1000);
+        or
+        $(".stream1").slideDown();
+        $(".stream1").slideDown(1000);
+        or
+        $(".stream1").slideUp();
+        $(".stream1").slideUp(1000);
+        or
+        $(".stream1").fadeTo(1000, 0.5);
    });
    $("#stream2_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
